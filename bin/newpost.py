d48f8e69380d743eb68edecb4ffd5c0566061f8d
Cong Ma <embolon@gmail.com>
2016-01-17 14:40:15 -0600

    adding an automatic new post script in python

---
+++ bin/newpost.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Arguments')
+parser.add_argument('-t', '--title', help='the title of this post',
+                    default='This is a test post')
+parser.add_argument('-c', '--categories', help='the categories of this post',
+                    default='default')
+parser.add_argument('-cm', '--comments', help='enable comments',
+                    action='store_true', default=False)
+parser.add_argument('-di', '--disqus-identifier', help='disqus identifier',
+                    default='false')
+parser.add_argument('-p', '--print', help='print the newly created post file name', 
+                    action='store_true', default=False)
+args = parser.parse_args()
+args_var = vars(args)
+
+if __name__ == '__main__':
+
+    # post name
+    today = time.strftime("%Y-%m-%d")
+    title = '-'.join(args_var['title'].lower().split())
+    post_name = today + '-' + title
+
+    # prepare post content
+    now = time.strftime("%Y-%m-%d %H:%M:%S %z")
+    disqus_identifier = args_var['disqus_identifier']
+    title = args_var['title']
+    categories = args_var['categories']
+    comments = 'true' if args_var['comments'] else 'false'
+    post_content = '''---
+layout: post
+title:  "{0}"
+date:   {1}
+categories: {2}
+comments: {3}
+disqus_identifier: {4}
+---
+'''.format(title, now, categories, comments, disqus_identifier)
+
+    # create new post file
+    with open('_posts/{0}.md'.format(post_name), 'w') as fh:
+       fh.write(post_content)
+
+    # print post file name
+    if args_var['print']:
+        print('_posts/{0}.md'.format(post_name))
