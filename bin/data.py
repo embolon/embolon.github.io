e6907a3ddfc0dc2fcfac05a79721ce7baedc5ece
Cong Ma <embolon@gmail.com>
2016-01-17 15:44:54 -0600

    adding a static data record using json file - static.json and its python parser

---
+++ bin/data.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import json
+import argparse
+import os.path
+
+parser = argparse.ArgumentParser(description='Argument')
+parser.add_argument('-di', '--disqus-identifier', help='return disqus identifier',
+                    action='store_true', default=False)
+parser.add_argument('-f', '--file', help='input Json File', default='default.json')
+args = parser.parse_args()
+args_var = vars(args)
+
+def initData():
+    newdata = {}
+    newdata['disqus_identifier'] = 0
+    return newdata
+
+if __name__ == '__main__':
+    
+    # read in the data
+    filepath = 'bin/{0}'.format(args_var['file'])
+    if not os.path.isfile(filepath):
+        data = initData()
+    else:
+        with open(filepath, 'r') as fh:
+            data = json.load(fh)
+    
+    # output the required data
+    if args_var['disqus_identifier']:
+        print('{0:012d}'.format(data['disqus_identifier']))
+        data['disqus_identifier'] += 1
+
+    # write back the data
+    with open(filepath, 'w') as fh:
+        json.dump(data, fh)
